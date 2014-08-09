a7e08d9785845e109b835b9cf553eb59ccda710f
nox-flamma <nickpops98@gmail.com>
2014-08-09 17:49:56 +1000

    Create the base of a easy to implement monster creation system and a battle system. NYI

--- Astaria/src/battle/Battle.java
+++ Astaria/src/battle/Battle.java
@@ -5,19 +5,39 @@ package battle;
  */
 public class Battle {
 
+    /** Who's turn it is
+     * 0 = Player
+     * 1 = First Monster
+     * 2 = Second Monster
+     * ect.
+     */
+    private static int battlePhase = 0;
 
     /**
      * Runs a battle with a monster.
      *
      * @param monster
      *        The monster the player will battle
+     * @param lvl
+     *        The level of the monster
      * @param ambush
      *        Checks whether the player was ambushed. If true the monster attacks first
      *
      * @return If the player won the battle
      */
-    public static boolean createBattleEncounter(Monster monster, boolean ambush) {
-
+    public static boolean createMonsterEncounter(int monster, int lvl, boolean ambush) {
+        int monsterBaseHealth = Math.round(Monster.monsterStats[monster][0] * ((lvl + 9)/10));
+        int monsterBaseAttack = Math.round(Monster.monsterStats[monster][1] * ((lvl + 9)/10));
+        int monsterBaseDefence = Math.round(Monster.monsterStats[monster][2] * ((lvl + 9)/10));
+        System.out.println("Level " + lvl + " " + Monster.monsterNames[monster] + " Encounter!");
+        if (ambush) {
+            System.out.println("You have been ambushed!");
+            battlePhase = 1;
+        }
+        //These next three line are temporary
+        System.out.println("HP: " + monsterBaseHealth);
+        System.out.println("AP: " + monsterBaseAttack);
+        System.out.println("DP: " + monsterBaseDefence);
         return true;
     }
 }
--- Astaria/src/battle/Monster.java
+++ Astaria/src/battle/Monster.java
@@ -4,15 +4,12 @@ package battle;
  * Created by Nicholas Donaldson on 9/08/2014.
  */
 public class Monster {
-    public static int level = 0;
-    public static int baseAttack = 0;
-    public static int baseHealth = 0;
-    public static int baseDefence = 0;
 
-    public Monster(int lvl, int ap, int hp, int dp) {
-        level = lvl;
-        baseAttack = ap;
-        baseHealth = hp;
-        baseDefence = dp;
-    }
+    public static String[] monsterNames = {"Goblin", "Witch", "Guard"};
+    public static int[][] monsterStats = {
+            /* Goblin */ {1, 2, 3},
+            /* Witch  */ {2, 3, 1},
+            /* Guard  */ {3, 1, 2}
+    };
+
 }
